d91c169b868ab05bdb547158a71c2ba72f143f68
Punya Sikri <punyasikri01@gmail.com>
2025-03-28 17:40:26 +0530

    Solution to Insertion Sort for Integers

--- Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -1,26 +1,17 @@
 // Your code here...
 int insertionSort(int arr[],int n){
     int i,j,temp;
-    int swapped;
-
     for (int i=0; i<n-1; i++){
-        swapped =0;
-        for (int j=0; j<n-i-1 ; j++){
-            if (arr[j] > arr[j+1]){
-                temp= arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-                swapped=1;
+        for (int j=0; j<n-i-1; j++){
+            if (arr[j]> arr[j+1]){
+                temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
             }
         }
-        if (swapped==0){
-            break;
-        }
+    }}
+ int printArray(int arr, int n){
+    for (int i=0; i<n; i++){
+        printf("%d",arr[i])
     }
-}
-int printArray(int arr[], int size){
-    for (int i=0; i<size; i++){
-        printf("%d ",arr[i]);
-    }
-    printf("\n");
-}
+ }   
